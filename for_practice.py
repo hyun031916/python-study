621e49f0b9b6430bb41038d40cd57e3efb148a8f
ksh <hyun031916@gmail.com>
2020-05-19 16:17:58 +0900

    369 게임

--- for_practice.py
+++ for_practice.py
@@ -10,18 +10,34 @@
 #         print(dan ,"X", i, "=", (dan * i)); #i:1-9
 #     print("----------")
 
-#N자리수의 각 자리 숫자합 출력하기 (for문 이용) 331=>7
-num = input("숫자를 입력하세요 : ")
-sum = 0
-for ch in num:
-    sum += int(ch)
-# for i in range(0, len(num)):
-#     n = num[i]
-#     #sum에 더하기
-#     sum+= int(n)
-#sum 출력
-print(sum)
+# #N자리수의 각 자리 숫자합 출력하기 (for문 이용) 331=>7
+# num = input("숫자를 입력하세요 : ")
+# sum = 0
+# for ch in num:
+#     sum += int(ch)
+# # for i in range(0, len(num)):
+# #     n = num[i]
+# #     #sum에 더하기
+# #     sum+= int(n)
+# #sum 출력
+# print(sum)
+
 #369 게임 1 2 짝 4 5 짝 7 8 짝 10 11 12 짝
+#1-10까지 반복
+#3으로 나누어 떨어지면 : "짝"
+for num in range(1, 40+1):
+    num = str(num)
+    is짝 = False
+    result =""
+    for n in num:
+        if n in "369":
+            result+="짝"
+            is짝 = True
+    if is짝 != True:
+        print(num)
+    else:
+        print(result)
+
 #전화번호부 만들고, 이름으로 검색하기
 #주차요금 계산하기, 최초 30분 2000원, 10분당 1000원, 25000원 넣을 수 있음
 #야구게임,
